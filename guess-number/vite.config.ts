c401bba0dd561471001dc86434e302eaddb7bc50
Yevhenii Riabokon <yevhenii.riabokon@dci-student.org>
2026-02-18 11:12:19 +0100

    in viteconfig added ./

--- guess-number/vite.config.ts
+++ guess-number/vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	base: '/', //
+	// base: './', (- for workflow and render)
+	base: './', // Use relative paths for assets
 	plugins: [react()],
 })
